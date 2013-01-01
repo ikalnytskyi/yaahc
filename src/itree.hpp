8be8eb47252f36da142b450bfa484be68f0927a5
Igor Kalnitsky <igor@kalnitsky.org>
2013-01-02 01:53:02 +0200

    Fix project name in LGPL headers.

--- src/itree.hpp
+++ src/itree.hpp
@@ -1,20 +1,20 @@
 /*
  * Copyright 2012 Igor Kalnitsky <igor@kalnitsky.org>
  *
- * This file is part of yaahc.
+ * This file is part of YaAHC.
  *
- * yaahc is free software: you can redistribute it and/or modify
+ * YaAHC is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of
  * the License, or (at your option) any later version.
  *
- * yaahc is distributed in the hope that it will be useful,
+ * YaAHC is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with yaahc.  If not, see <http://www.gnu.org/licenses/>.
+ * along with YaAHC.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef YAAHC_ITREE_HPP_
