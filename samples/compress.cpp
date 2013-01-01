0dde4f1863816747942730c4e004b930c3fce2d8
Igor Kalnitsky <igor@kalnitsky.org>
2013-01-02 01:46:53 +0200

    Add samples: compress/uncompress utils.

---
+++ samples/compress.cpp
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2012 Igor Kalnitsky <igor@kalnitsky.org>
+ *
+ * This file is part of yaahc.
+ *
+ * yaahc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * yaahc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with yaahc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+#include <ahc/tree.hpp>
+
+
+using ahc::byte_t;
+using ahc::bit_t;
+
+void compress(std::istream& in, std::ostream& out);
+void flush_buffer(std::vector<bit_t>& w_buffer, std::ostream& out);
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 3)
+    {
+        std::cerr << "Help: compress <source> <destination>" << std::endl;
+        return -1;
+    }
+
+    const char* src = argv[1];
+    const char* dst = argv[2];
+
+    std::ifstream in(src, std::ios::binary);
+    std::ofstream out(dst, std::ios::binary);
+
+    compress(in, out);
+    return 0;
+}
+
+
+void compress(std::istream& in, std::ostream& out)
+{
+    const size_t BUFFER_SIZE = 512;
+
+    std::vector<byte_t> r_buffer(BUFFER_SIZE);
+    std::vector<bit_t>  w_buffer;
+
+    ahc::Tree tree(ahc::FGK);
+    w_buffer.reserve(8 * BUFFER_SIZE + BUFFER_SIZE);
+
+    while (in)
+    {
+        in.read(reinterpret_cast<char*>(r_buffer.data()), BUFFER_SIZE);
+        size_t read_bytes = in.gcount();
+
+        // encode read bytes
+        for (size_t i = 0; i < read_bytes; ++i)
+        {
+            std::vector<bit_t> code = tree.encode(r_buffer[i]);
+            std::copy(std::begin(code), std::end(code), std::back_inserter(w_buffer));
+        }
+
+        // flush buffer if it almost fill
+        if (w_buffer.size() >= 8 * BUFFER_SIZE)
+            flush_buffer(w_buffer, out);
+    }
+
+    // fill buffer with zeroes if byte is not complete
+    byte_t extra_bits = 0;
+    if (w_buffer.size() % 8 != 0)
+    {
+        extra_bits = 8 - w_buffer.size() % 8;
+        std::fill_n(std::back_inserter(w_buffer), extra_bits, 0);
+        flush_buffer(w_buffer, out);
+    }
+
+    out.write(reinterpret_cast<char*>(&extra_bits), 1);
+}
+
+
+void flush_buffer(std::vector<ahc::bit_t>& w_buffer, std::ostream& out)
+{
+    std::vector<byte_t> bytes;
+    bytes.reserve(w_buffer.size() / 8);
+
+    for (size_t i = 0; i + 8 <= w_buffer.size(); i += 8)
+    {
+        byte_t byte = 0;
+        for (size_t j = 0; j < 8; ++j)
+            byte |= w_buffer[i + j] << (7 - j);
+        bytes.push_back(byte);
+    }
+
+    w_buffer.erase(std::begin(w_buffer), std::begin(w_buffer) + bytes.size() * 8);
+    out.write(reinterpret_cast<char*>(bytes.data()), bytes.size());
+}
---
+++ samples/uncompress.cpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2012 Igor Kalnitsky <igor@kalnitsky.org>
+ *
+ * This file is part of yaahc.
+ *
+ * yaahc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * yaahc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with yaahc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+#include <ahc/tree.hpp>
+
+using ahc::byte_t;
+using ahc::bit_t;
+
+void uncompress(std::istream& in, std::ostream& out);
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 3)
+    {
+        std::cerr << "Help: uncompress <source> <destination>" << std::endl;
+        return -1;
+    }
+
+    const char* src = argv[1];
+    const char* dst = argv[2];
+
+    std::ifstream in(src, std::ios::binary);
+    std::ofstream out(dst, std::ios::binary);
+
+    uncompress(in, out);
+    return 0;
+}
+
+
+void uncompress(std::istream& in, std::ostream& out)
+{
+    const size_t BUFFER_SIZE = 512;
+
+    std::vector<byte_t> r_buffer(BUFFER_SIZE);
+    std::vector<bit_t>  w_buffer;
+
+    ahc::Tree tree(ahc::FGK);
+    w_buffer.reserve(8 * BUFFER_SIZE + BUFFER_SIZE);
+
+    in.seekg(0, std::ios::end);
+    std::streampos eof = in.tellg();
+    in.seekg(0, std::ios::beg);
+
+    while (in)
+    {
+        in.read(reinterpret_cast<char*>(r_buffer.data()), BUFFER_SIZE);
+        size_t read_bytes = in.gcount();
+
+        for (size_t i = 0; i < read_bytes; ++i)
+        {
+            std::vector<bit_t> byte(8);
+            for (size_t j = 0; j < 8 * sizeof(byte_t); ++j)
+                byte[j] = (0x1 & (r_buffer[i] >> (7 - j)));
+            std::copy(std::begin(byte), std::end(byte), std::back_inserter(w_buffer));
+        }
+
+        if (in.tellg() >= eof || in.eof())
+        {
+            byte_t extra_bits = r_buffer[read_bytes - 1];
+            w_buffer.resize(w_buffer.size() - extra_bits - 8);
+        }
+
+        std::vector<byte_t> output = tree.decode(w_buffer);
+        out.write(reinterpret_cast<char*>(output.data()), output.size());
+        w_buffer.clear();
+    }
+}
